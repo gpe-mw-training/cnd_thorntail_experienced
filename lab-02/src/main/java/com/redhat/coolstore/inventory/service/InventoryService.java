35d1e505bd184c60d5f19feaeff7b92813168e46
Chad Darby <cdarby@redhat.com>
2019-01-22 22:51:43 -0500

    lab-02 solution

--- lab-02/src/main/java/com/redhat/coolstore/inventory/service/InventoryService.java
+++ lab-02/src/main/java/com/redhat/coolstore/inventory/service/InventoryService.java
@@ -12,9 +12,15 @@ public class InventoryService {
     @PersistenceContext(unitName = "primary")
     private EntityManager entityManager;
     
+    
     public Inventory getInventory(String itemId) {
+
+    	  // TODO:
+    	  // - Add code to return the inventory object with the given itemId
+    	  // - Use the entityManager.find(...) method
 
     	  return entityManager.find(Inventory.class, itemId);
 
-   	}    
+    	}
+
 }
--- lab-02/src/test/java/com/redhat/coolstore/inventory/service/InventoryServiceTest.java
+++ lab-02/src/test/java/com/redhat/coolstore/inventory/service/InventoryServiceTest.java
@@ -1,6 +1,9 @@
 package com.redhat.coolstore.inventory.service;
 
-import java.util.Properties;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import javax.inject.Inject;
 
@@ -11,61 +14,46 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.arquillian.CreateSwarm;
 
 import com.redhat.coolstore.inventory.model.Inventory;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(Arquillian.class)
 public class InventoryServiceTest {
 
-    @Inject
-    private InventoryService inventoryService;
-    
-    private static String port="18081";
+	@Inject
+	private InventoryService inventoryService;
 
-    @CreateSwarm
-    public static Swarm newContainer() throws Exception {
-    	
-        Properties properties = new Properties();
-        properties.put("swarm.http.port", port);
-        return new Swarm(properties).withProfile("local");
-    }
-    
     @Deployment
     public static Archive<?> createDeployment() {
-
-    		return ShrinkWrap.create(WebArchive.class)
+        return ShrinkWrap.create(WebArchive.class)
                 .addPackages(true, InventoryService.class.getPackage())
                 .addPackages(true, Inventory.class.getPackage())
-                .addAsResource("project-local.yml", "project-local.yml")
+                .addAsResource("project-local.yml", "project-defaults.yml")
                 .addAsResource("META-INF/test-persistence.xml",  "META-INF/persistence.xml")
                 .addAsResource("META-INF/test-load.sql",  "META-INF/test-load.sql");
     }
     
-    @Test
-    public void getInventory() throws Exception {
-    	
-        assertThat(inventoryService, notNullValue());
-        Inventory inventory = inventoryService.getInventory("123456");
-        assertThat(inventory, notNullValue());
-        assertThat(inventory.getQuantity(), is(99));
-    }   
-    
+	@Test
+	public void getInventory() throws Exception {
+		assertThat(inventoryService, notNullValue());
+		Inventory inventory = inventoryService.getInventory("123456");
+		assertThat(inventory, notNullValue());
+		assertThat(inventory.getQuantity(), is(99));
+	}
+	
     @Test
     public void getNonExistingInventory() throws Exception {
 
-        assertThat(inventoryService, notNullValue());
+        // TODO:
+        //
+        // - assert that the inventory service is not null
+		assertThat(inventoryService, notNullValue());
 
-        Inventory inventory = inventoryService.getInventory("99999");
+		// - get an inventory object using a non-existant id: "99999"
+		Inventory inventory = inventoryService.getInventory("99999");
 
-        assertThat(inventory, nullValue());
+		// - assert that the inventory object retrieved is a null value
+		assertThat(inventory, nullValue());
 
-    }    
-    
+    }
 }
