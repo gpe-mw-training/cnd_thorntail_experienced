1d1b223087d4f68b2b0b773f9685c1cef8bf1850
Chad Darby <cdarby@redhat.com>
2019-01-23 02:30:06 -0500

    lab-03 solution

--- lab-03/src/main/java/com/redhat/coolstore/inventory/rest/InventoryResource.java
+++ lab-03/src/main/java/com/redhat/coolstore/inventory/rest/InventoryResource.java
@@ -9,41 +9,44 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import com.redhat.coolstore.inventory.model.Inventory;
 import com.redhat.coolstore.inventory.service.InventoryService;
 
+@Api(value = "/inventory", description = "Inventory Resources", tags = "inventory")
 @Path("/inventory")
 @ApplicationScoped
 public class InventoryResource {
 
-	  @Inject
-	  private InventoryService inventoryService;
+      @Inject
+      private InventoryService inventoryService;
 
-	  
-	    @GET
-	    @Path("/{itemId}")
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public Inventory getInventory(@PathParam("itemId") String itemId) {
+      
+        @GET
+        @Path("/{itemId}")
+        @Produces(MediaType.APPLICATION_JSON)
+        @ApiOperation(value = "Get an inventory item",
+            notes = "Returns an Inventory resource",
+            response = Inventory.class
+        ) 
+        public Inventory getInventory(@PathParam("itemId") String itemId) {
 
-	        //
-	        // TODO
-	        //
+            // get the inventory object from the inventoryService. Use the given itemId.
+            Inventory inventory = inventoryService.getInventory(itemId);
 
-	        // get the inventory object from the inventoryService. Use the given itemId.
-	    		Inventory inventory = inventoryService.getInventory(itemId);
-
-	        // if the inventory object that you retrieved is null then
-	        // throw a NotFoundException
-	        // else return the inventory object
-	    		
-	    		if (inventory == null) {
-	    			throw new NotFoundException("Item id not found: " + itemId);
-	    		}
-	    		else {
-	    			return inventory;
-	    		}
-	    		
-	    	
-	    }
-	    
+            // if the inventory object that you retrieved is null then
+            // throw a NotFoundException
+            // else return the inventory object
+                
+            if (inventory == null) {
+                throw new NotFoundException("Item id not found: " + itemId);
+            } else {
+                return inventory;
+            }
+                
+            
+        }
+        
 }
