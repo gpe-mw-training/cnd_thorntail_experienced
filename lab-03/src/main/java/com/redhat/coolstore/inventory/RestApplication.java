ef059ff1a84f365eba30a35ddc73614c93722dc7
Chad Darby <cdarby@redhat.com>
2019-03-05 02:18:31 +0900

    cleanup

--- lab-03/src/main/java/com/redhat/coolstore/inventory/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.coolstore.inventory;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestApplication extends Application {
-}
